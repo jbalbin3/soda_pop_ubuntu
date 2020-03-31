b1ca743cb928a85acb171c39de505d4145221f9b
John Balbin <jbalbin3@yahoo.com>
2020-03-31 11:40:09 -0700

    added hidden files

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": ["airbnb", "airbnb/hooks"],
+  "env": {
+    "browser": true,
+    "node": true,
+    "jest": true,
+    "commonjs": true
+    },
+  // "rules": {
+  //   "no-console": "off",
+  //   }
+ };
